00043421a3a787b69d586d6771bf6cbfa02af994
roeckerdiego <diego.bram.oliveira@escola.pr.gov.br>
2025-05-22 14:51:27 +0000

    teste02

--- teste.py
+++ teste.py
@@ -1,1 +1,8 @@
-print("olá mundo") 
+curso01 = int(input("digite quantas avaliações o curso01 teve"))
+curso02 = int(input("digite quantas avaliações o curso02 teve"))
+if curso01 == curso02:
+    print("empate")
+elif curso01 > curso02:
+    print("curso01 é maior")
+else:
+    print("curso02 é maior") 
